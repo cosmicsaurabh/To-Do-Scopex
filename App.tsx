1ff838ab0dea7b3496d4a6217e7b851adcfef731
cosmicsaurabh <saurabhyadavclg20@gmail.com>
2024-09-04 00:12:57 +0530

    More error handling

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
-import React, {useEffect, useState} from 'react';
-import {AuthProvider} from './context/AuthProvider';
+import React, { useEffect, useState } from 'react';
+import { AuthProvider } from './context/AuthProvider';
 import RootNavigator from './components/Navigation/RootNavigator';
 import SplashScreen from './components/SplashScreen/SplashScreen';
 import Toast from 'react-native-toast-message';
@@ -8,28 +8,31 @@ import ThemeProvider from './context/ThemeProvider';
 
 function App(): JSX.Element {
   const [showSplash, setShowSplash] = useState(true);
+
   useEffect(() => {
     const timer = setTimeout(() => {
       setShowSplash(false);
-    },500);
+    }, 500);
 
     return () => clearTimeout(timer);
   }, []);
+
   if (showSplash) {
-    return <SplashScreen />; 
+    return <SplashScreen />;
   }
 
   return (
-    <AuthProvider>
-      <ThemeProvider>
-        <TodoProvider>
-          <RootNavigator />
-          <Toast />
-        </TodoProvider>
-      </ThemeProvider>
-    </AuthProvider>
+    <>
+      <AuthProvider>
+        <ThemeProvider>
+          <TodoProvider>
+            <RootNavigator />
+          </TodoProvider>
+        </ThemeProvider>
+      </AuthProvider>
+      <Toast /> 
+    </>
   );
 }
 
 export default App;
-
--- context/TodoProvider.tsx
+++ context/TodoProvider.tsx
@@ -1,13 +1,13 @@
-import React, { createContext, useState, useContext, useEffect } from 'react';
+import React, {createContext, useState, useContext, useEffect} from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import UUID from 'react-native-uuid';
-import { useAuth } from './AuthProvider';
-import Toast from 'react-native-toast-message'; 
+import {useAuth} from './AuthProvider';
+import Toast from 'react-native-toast-message';
 
 const TodoContext = createContext();
 
-const TodoProvider = ({ children }) => {
-  const { user } = useAuth();
+const TodoProvider = ({children}) => {
+  const {user} = useAuth();
   const [todos, setTodos] = useState([]);
   const [page, setPage] = useState(1);
   const [allTodos, setAllTodos] = useState([]);
@@ -17,7 +17,7 @@ const TodoProvider = ({ children }) => {
   useEffect(() => {
     fetchInitialTodos();
   }, [user]);
-  const showToast = (message) => {
+  const showToast = message => {
     Toast.show({
       type: 'error',
       position: 'bottom',
@@ -26,9 +26,17 @@ const TodoProvider = ({ children }) => {
     });
   };
   const fetchInitialTodos = async () => {
+    await wait(200); // Simulate network delay
+    if (Math.random() < 0.2) {
+      const randomError = 'Random error occurred during initial fetch';
+      console.error(randomError);
+      showToast(randomError);
+      return;
+    }
     if (user) {
       try {
-        const storedUsers = JSON.parse(await AsyncStorage.getItem('users')) || [];
+        const storedUsers =
+          JSON.parse(await AsyncStorage.getItem('users')) || [];
         const currentUser = storedUsers.find(u => u.email === user.email);
         const userTodos = currentUser ? currentUser.todos || [] : [];
         setAllTodos(userTodos);
@@ -42,24 +50,37 @@ const TodoProvider = ({ children }) => {
     }
   };
 
-  const loadMoreTodos = () => {
+  const loadMoreTodos = async() => {
+    await wait(200); // Simulate network delay
+    if (Math.random() < 0.2) {
+      const randomError = 'Random error occurred during loadmore ';
+      console.error(randomError);
+      showToast(randomError);
+      return;
+    }
     try {
       const newPage = page + 1;
       const newTodos = allTodos.slice(0, newPage * ITEMS_PER_PAGE);
-      setTodos(prevTodos => [...prevTodos, ...newTodos.slice(prevTodos.length)]);
+      setTodos(prevTodos => [
+        ...prevTodos,
+        ...newTodos.slice(prevTodos.length),
+      ]);
       setPage(newPage);
     } catch (err) {
       console.error('Error loading more todos:', err);
       setError('Failed to load more todos. Please try again.');
       showToast('Failed to load more todos. Please try again.');
     }
-  }
+  };
 
-  const saveTodos = async (updatedTodos) => {
+  const saveTodos = async updatedTodos => {
     if (user) {
       try {
-        const storedUsers = JSON.parse(await AsyncStorage.getItem('users')) || [];
-        const currentUserIndex = storedUsers.findIndex(u => u.email === user.email);
+        const storedUsers =
+          JSON.parse(await AsyncStorage.getItem('users')) || [];
+        const currentUserIndex = storedUsers.findIndex(
+          u => u.email === user.email,
+        );
         if (currentUserIndex !== -1) {
           storedUsers[currentUserIndex].todos = updatedTodos;
           await AsyncStorage.setItem('users', JSON.stringify(storedUsers));
@@ -74,9 +95,11 @@ const TodoProvider = ({ children }) => {
     }
   };
 
-  const bookmarkTodoItem = async (updatedTodo) => {
+  const bookmarkTodoItem = async updatedTodo => {
     try {
-      const updatedTodos = allTodos.map(todo => todo.id === updatedTodo.id ? updatedTodo : todo);
+      const updatedTodos = allTodos.map(todo =>
+        todo.id === updatedTodo.id ? updatedTodo : todo,
+      );
       await saveTodos(updatedTodos);
     } catch (err) {
       console.error('Error bookmarking todo:', err);
@@ -84,10 +107,20 @@ const TodoProvider = ({ children }) => {
       showToast('Failed to bookmark todo. Please try again.');
     }
   };
-  
-  const addTodoItem = async (title,bookmarked) => {
+
+  const addTodoItem = async (title, bookmarked) => {
+    if (title.length < 3) {
+      throw new Error('Title must be at least 3 characters long');
+    }
+    await wait(200); // Simulate network delay
+    if (Math.random() < 0.2) {
+      const randomError = 'Random error occurred during add To-Do';
+      console.error(randomError);
+      showToast(randomError);
+      return;
+    }
     try {
-      const newTodo = { id: UUID.v4(), title, done: false, bookmarked };
+      const newTodo = {id: UUID.v4(), title, done: false, bookmarked};
       const updatedTodos = [...allTodos, newTodo];
       await saveTodos(updatedTodos);
     } catch (err) {
@@ -96,10 +129,19 @@ const TodoProvider = ({ children }) => {
       showToast('Failed to add todo. Please try again.');
     }
   };
-  
-  const updateTodoItem = async (updatedTodo) => {
+
+  const updateTodoItem = async updatedTodo => {
+    await wait(200); // Simulate network delay
+    if (Math.random() < 0.2) {
+      const randomError = 'Random error occurred during updating To-Do';
+      console.error(randomError);
+      showToast(randomError);
+      return;
+    }
     try {
-      const updatedTodos = allTodos.map(todo => todo.id === updatedTodo.id ? updatedTodo : todo);
+      const updatedTodos = allTodos.map(todo =>
+        todo.id === updatedTodo.id ? updatedTodo : todo,
+      );
       await saveTodos(updatedTodos);
     } catch (err) {
       console.error('Error updating todo:', err);
@@ -107,8 +149,15 @@ const TodoProvider = ({ children }) => {
       showToast('Failed to update todo. Please try again.');
     }
   };
-  
-  const deleteTodoItem = async (id) => {
+
+  const deleteTodoItem = async id => {
+    await wait(200); // Simulate network delay
+    if (Math.random() < 0.2) {
+      const randomError = 'Random error occurred during Deleting To-Do';
+      console.error(randomError);
+      showToast(randomError);
+      return;
+    }
     try {
       const updatedTodos = allTodos.filter(todo => todo.id !== id);
       await saveTodos(updatedTodos);
@@ -120,13 +169,22 @@ const TodoProvider = ({ children }) => {
   };
 
   return (
-    <TodoContext.Provider value={{ todos, loadMoreTodos, addTodoItem, updateTodoItem, deleteTodoItem, bookmarkTodoItem}}>
+    <TodoContext.Provider
+      value={{
+        todos,
+        loadMoreTodos,
+        addTodoItem,
+        updateTodoItem,
+        deleteTodoItem,
+        bookmarkTodoItem,
+      }}>
       {children}
     </TodoContext.Provider>
   );
 };
-
+export async function wait(ms: number) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
 export const useTodo = () => useContext(TodoContext);
 
 export default TodoProvider;
-
